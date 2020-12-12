09b30953c413addb792069fb549140081cff56b3
Florian SIMUNEK <florian.simunek@gmail.com>
2020-12-12 01:11:54 +0100

    Added ScrollSpy + smooth scrolling

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".navbar a, footer a").on("click", function (event) {
+        event.preventDefault();
+        let hash = this.hash;
+        console.log(hash)
+        $("body,html").animate({ scrollTop: $(hash).offset().top-150 }, 900, function () { window.location.hash = $(hash).offset().top-150; })
+        console.log({scrollTop: $(hash).offset().top-150})
+    })
+})
