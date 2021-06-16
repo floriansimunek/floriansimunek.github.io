890571ff9cf42b33a8218f94b95f7b4c1114cffb
Florian <florian.simunek@gmail.com>
2021-06-16 21:25:44 +0200

    Delete windows location in smooth scrolling script

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
 ////////////////////smooth scrolling////////////////////////
 $(function () {
-    $(".navbar a, .footer a").on("click", function (e) {
-        e.preventDefault();
-        let hash = this.hash;
-        console.log(hash)
-        $("body,html").animate({ scrollTop: $(hash).offset().top-150 }, 900, function () { window.location.hash = $(hash).offset().top-150; })
-        console.log({scrollTop: $(hash).offset().top-150})
-    })
-})
+  $(".navbar a, .footer a").on("click", function (e) {
+    e.preventDefault();
+    let hash = this.hash;
+    $("body,html").animate({ scrollTop: $(hash).offset().top - 150 }, 900, function () {});
+  });
+});
 ///////////////////////////////////////////////////////////
 
 //////////////////////contact form/////////////////////////
-$(document).ready(function(){
-    $('#submit').submit(function(e){
-        let email = $('.email').val();
-        let subject = $('.subject').val();
-        let message = $('.message').val();
-        let errorMessage = $('.status');
-        errorMessage.empty();
+$(document).ready(function () {
+  $("#submit").submit(function (e) {
+    let email = $(".email").val();
+    let subject = $(".subject").val();
+    let message = $(".message").val();
+    let errorMessage = $(".status");
+    errorMessage.empty();
 
-        if(email.length > 5 && email.includes('@') && email.includes('.')){
-            errorMessage.append("<div class='message'>L'adresse mail est valide</div>");
-        } else {
-            e.preventDefault();
-            errorMessage.append("<div class='message'>L'adresse mail n'est pas valide</div>");
-        }
+    if (email.length > 5 && email.includes("@") && email.includes(".")) {
+      errorMessage.append("<div class='message'>L'adresse mail est valide</div>");
+    } else {
+      e.preventDefault();
+      errorMessage.append("<div class='message'>L'adresse mail n'est pas valide</div>");
+    }
 
-        if(subject.length > 2){
-            errorMessage.append("<div class='message'>Le sujet est valide</div>");
-        } else {
-            e.preventDefault();
-            errorMessage.append("<div class='message'>Le sujet n'est pas valide</div>");
-        }
+    if (subject.length > 2) {
+      errorMessage.append("<div class='message'>Le sujet est valide</div>");
+    } else {
+      e.preventDefault();
+      errorMessage.append("<div class='message'>Le sujet n'est pas valide</div>");
+    }
 
-        if(message.length >= 10){
-            errorMessage.append("<div class='message'>Le message est valide</div>");
-        } else {
-            e.preventDefault();
-            errorMessage.append("<div class='message'>Le message n'est pas valide</div>");
-        }
+    if (message.length >= 10) {
+      errorMessage.append("<div class='message'>Le message est valide</div>");
+    } else {
+      e.preventDefault();
+      errorMessage.append("<div class='message'>Le message n'est pas valide</div>");
+    }
 
-        window.location.reload();
-    })
-})
+    window.location.reload();
+  });
+});
 ///////////////////////////////////////////////////////////
 
 /////////////////////////Animations////////////////////////
