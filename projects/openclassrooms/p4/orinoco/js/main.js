e0e90331be6c63545381766c9a50eff055463119
Florian <florian.simunek@gmail.com>
2021-07-14 13:49:42 +0200

    Delete old version

--- projects/openclassrooms/p4/orinoco/js/main.js
+++
@@ -1,9 +0,0 @@
-//Récupération de l'id du produit dans l'url pour l'affichage (quand on clique sur le produit (view-product.html))
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const productId = urlParams.get('product_id');
-
-//Création des objets
-let product = new Product();
-let cart = new Cart();
-let order = new Order();
